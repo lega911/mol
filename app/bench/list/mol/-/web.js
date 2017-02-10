53a30faba4a343e11eb558542c921378a5374ebc
nin-jin <nin-jin@ya.ru>
2017-02-10 13:32:43 +0300

    new build

--- app/bench/list/mol/-/web.js
+++ app/bench/list/mol/-/web.js
@@ -513,8 +513,6 @@ var $;
             var next_normal = this.normalize(next, this._next);
             if (next_normal === this._next)
                 return next_normal;
-            if (next_normal === this.host[this.field])
-                return next_normal;
             this._next = next_normal;
             this.obsolete();
             return this.get();
